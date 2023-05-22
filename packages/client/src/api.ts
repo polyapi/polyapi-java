01b493cd519eef5d4ba7da78afcfe66abf34cc50
Vladimir Hrusovsky <vladimir.hrusovsky@hotovo.com>
2023-05-22 08:59:30 +0200

    Behind proxy

--- packages/client/src/api.ts
+++ packages/client/src/api.ts
@@ -1,10 +1,21 @@
-import axios from 'axios';
+import Axios from 'axios';
+import { HttpProxyAgent } from 'http-proxy-agent';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import {
   HEADER_ACCEPT_FUNCTION_DEFINITION,
   HEADER_ACCEPT_WEBHOOK_HANDLE_DEFINITION,
   POLY_HEADER_API_KEY,
 } from './constants';
 import { FunctionDefinitionDto, Specification, WebhookHandleDefinitionDto } from '@poly/common';
+
+const httpProxy = process.env.HTTP_PROXY || process.env.http_proxy || process.env.npm_config_proxy;
+const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy || process.env.npm_config_https_proxy;
+
+const axios = Axios.create({
+  httpAgent: httpProxy ? new HttpProxyAgent(httpProxy) : undefined,
+  httpsAgent: httpsProxy ? new HttpsProxyAgent(httpsProxy, { rejectUnauthorized: false }) : undefined,
+  proxy: false,
+});
 
 export const getSpecs = async (contexts?: string[], names?: string[], ids?: string[]) => {
   return (
@@ -15,8 +26,8 @@ export const getSpecs = async (contexts?: string[], names?: string[], ids?: stri
       params: {
         contexts,
         names,
-        ids
-      }
+        ids,
+      },
     })
   ).data;
 };
