47f80f0a27fb69181e4a9064eb9abe5876d674bd
Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>
2023-06-15 16:49:40 -0300

    Prefix `openai_` config variable records.

--- prisma/migrations/20230614013445_add_multilevel_config_variable/migration.sql
+++ prisma/migrations/20230614013445_add_multilevel_config_variable/migration.sql
@@ -1,5 +1,6 @@
 -- RedefineTables
-PRAGMA foreign_keys=OFF;
+PRAGMA foreign_keys = OFF;
+
 CREATE TABLE "new_config_variable" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -10,8 +11,27 @@ CREATE TABLE "new_config_variable" (
     CONSTRAINT "config_variable_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "tenant" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "config_variable_environmentId_fkey" FOREIGN KEY ("environmentId") REFERENCES "environment" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_config_variable" ("created_at", "id", "name", "value") SELECT "created_at", "id", "name", "value" FROM "config_variable";
+
+INSERT INTO
+    "new_config_variable" ("created_at", "id", "name", "value")
+SELECT
+    "created_at",
+    "id",
+    "name",
+    "value"
+FROM
+    "config_variable";
+
 DROP TABLE "config_variable";
-ALTER TABLE "new_config_variable" RENAME TO "config_variable";
+
+ALTER TABLE
+    "new_config_variable" RENAME TO "config_variable";
+
 PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+
+PRAGMA foreign_keys = ON;
+
+UPDATE
+    config_variable
+SET
+    name = 'openai_' || name;
