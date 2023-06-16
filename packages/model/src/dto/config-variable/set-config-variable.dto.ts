99a69142eb361c3d5c51621cae46303804f9d006
Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>
2023-06-16 13:30:16 -0300

    Config variables to pascal case.

--- packages/model/src/dto/config-variable/set-config-variable.dto.ts
+++ packages/model/src/dto/config-variable/set-config-variable.dto.ts
@@ -1,9 +1,9 @@
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export enum ConfigVariableName {
-  openai_keyword_similarity_threshold = 'openai_keyword_similarity_threshold',
-  openai_function_match_limit = 'openai_function_match_limit',
-  openai_extract_keywords_temperature = 'openai_extract_keywords_temperature',
+  OpenAIKeywordSimilarityThreshold = 'OpenAIKeywordSimilarityThreshold',
+  OpenAIFunctionMatchLimit = 'OpenAIFunctionMatchLimit',
+  OpenAIExtractKeywordsTemperature = 'OpenAIExtractKeywordsTemperature',
 }
 
 export class SetConfigVariableDto {
