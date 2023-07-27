a3f10be94f302889c0c7340ca68b5251baab0659
Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>
2023-07-27 14:15:04 -0300

    Untrack files.

--- packages/client/publish_develop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VERSION=$(node -p "require('./package.json').version")
-echo "Publishing version $VERSION..."
-npm publish --tag develop
--- packages/client/publish_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION=$(node -p "require('./package.json').version")
-echo "Unpublishing version $VERSION..."
-npm unpublish --registry http://localhost:4873/ polyapi@$VERSION --force
-echo "Publishing version $VERSION..."
-npm publish --registry http://localhost:4873/
