3cb2dccc2ee47b15fcf15173e4cb34e05c47ad58
Adrián Locurcio <adrianlocurcio@MacBook-Pro-de-Admin.local>
2023-08-02 23:38:03 -0300

    Done corrections.

--- prisma/migrations/20230803023201_add_unique_created_at_into_conversation_message/migration.sql
+++ prisma/migrations/20230803023201_add_unique_created_at_into_conversation_message/migration.sql
@@ -4,8 +4,5 @@
   - A unique constraint covering the columns `[createdAt]` on the table `conversation_message` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX "Conversation_createdAt_key";
-
 -- CreateIndex
 CREATE UNIQUE INDEX "conversation_message_createdAt_key" ON "conversation_message"("createdAt");
